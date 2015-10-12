0b749b7e34bb3a2740f8c0d721706f202b2bfd28
ignazio-castrogiovanni <email@ignazio-castrogiovanni.com>
2015-10-13 09:40:27 +1100

    Refactoring through JSHint validation

--- main.js
+++ main.js
@@ -1,123 +1,95 @@
-var operationWaiting = false;
-var operation = "";
-var currNum = 0;
-var firstValue = true;
-var dotPressed = false;
-var currValue = 0;
+// onClick functions
+var numberClicked;
+var operationClicked;
+var dotClicked;
+var clearDisplay;
 
-function add(a, b) {
-  return a + b;
-}
+(function() {
+  "use strict";
+  var operationWaiting = false;
+  var operation = "";
+  var currNum = 0;
+  var firstValue = true;
+  var dotPressed = false;
+  var currValue = 0;
 
-function multiply(a, b) {
-  return a * b;
-}
+  numberClicked = function(num) {
+    // Update internal value
+    if (currNum === 0) {
+      currNum = num;
+    } else {
 
-function divide(a, b) {
-  return a / b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function percent(a) {
-  return a/100;
-}
-
-function numberClicked(num) {
-
-
-
-  // Update internal value
-  if(currNum == 0) {
-    currNum = num;
-  } else {
-
-    // So check if it's already decimal
-    var decimalNum = (currNum % 1 != 0);
-    console.log("Decimal num? " + decimalNum);
-    // Dot has just been pressed. For once we allowed the dot ;)
-    if(!decimalNum && dotPressed) {
-      currNum = currNum + ".";
-      dotPressed = false;
+      // So check if it's already decimal
+      var decimalNum = (currNum % 1 !== 0);
+      // Dot has just been pressed. For once we allowed the dot ;)
+      if (!decimalNum && dotPressed) {
+        currNum = currNum + ".";
+        dotPressed = false;
+      }
+      currNum = Number(currNum.toString() + num.toString());
     }
-    currNum = Number(currNum.toString() + num.toString());
-    console.log("current number: " + currNum);
-  }
 
-  // Show internal value
-  console.log("currValue: " + currValue);
-  console.log("currNum: " + currNum);
+    // Display current number
+    updateDisplay(currNum);
+  };
 
-  // Display current number
-  updateDisplay(currNum);
-}
+  operationClicked = function(operator) {
 
-function operationClicked(operator) {
-
-  if(currValue == 0) {
+    if (currValue === 0) {
       currValue = currNum;
     }
 
-  // If we already pressed the operation button for the first time, then we perform the previous operation.
-  if(operationWaiting) {
-    executeOperation(currNum);
+    // If we already pressed the operation button for the first time, then we perform the previous operation.
+    if (operationWaiting) {
+      executeOperation(currNum);
+    }
+    currNum = 0;
+    updateDisplay(currValue); // To Write
+    operationWaiting = true;
+    operation = operator;
+  };
+
+  clearDisplay = function() {
+    operationWaiting = false;
+    operation = "";
+    currNum = 0;
+    firstValue = true;
+    currValue = 0;
+    updateDisplay(0);
+  };
+
+  dotClicked = function() {
+    if (currNum === 0) {
+      return;
+    }
+    dotPressed = true;
+
+    updateDisplay(currNum + ".");
+  };
+
+  function executeOperation(num) {
+    switch (operation) {
+      case '/':
+        currValue = currValue / num;
+        break;
+      case 'X':
+        currValue = currValue * num;
+        break;
+      case '-':
+        currValue = currValue - num;
+        break;
+      case '+':
+        currValue = currValue + num;
+        break;
+      case '%':
+        currValue = currValue / 100;
+        break;
+      default:
+    }
   }
-  currNum = 0;
-  updateDisplay(currValue); // To Write
-  operationWaiting = true;
-  operation = operator;
-}
 
-function clearDisplay() {
-  operationWaiting = false;
-  operation = "";
-  currNum = 0;
-  firstValue = true;
-  currValue = 0;
-  updateDisplay(0);
-}
-function dotClicked() {
-  if(currNum == 0) {
-    return;
+  function updateDisplay(num) {
+    var displayElem = document.getElementById("display");
+    displayElem.innerHTML = num;
   }
-  dotPressed = true;
-  console.log(".");
-
-  updateDisplay(currNum + ".");
-}
-function equalClicked() {
-  alert("=");
-}
-
-function executeOperation(num) {
-  console.log("operation: " + currValue + " " + operation + " " + num);
-
-  switch(operation) {
-    case '/': {
-        currValue = divide(currValue, num);
-      } break;
-    case 'X': {
-      currValue = multiply(currValue, num);
-    } break;
-    case '-': {
-      currValue = subtract(currValue, num);
-    } break;
-    case '+': {
-      currValue = add(currValue, num);
-    } break;
-    case '%': {
-      currValue = percent(currValue);
-    } break;
-    case '=': {
-      updateDisplay(currValue);
-    } break;
-
-    default:
-  }
-}
-function updateDisplay(num) {
-  var displayElem = document.getElementById("display");
-  display.innerHTML = num;
-}
+})();
